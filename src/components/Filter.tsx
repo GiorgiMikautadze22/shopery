3d2b43bcc6c020109a85a379ce2b80f7c6f4641c
creatorm22 <mikautadze.giorgi2223@gmail.com>
2023-11-16 23:27:28 +0400

    added filter design and custom checkboxes

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -18,6 +18,33 @@ const FilterButton = styled.button`
   gap: 10px;
 `;
 
+const StyledInput = styled.input`
+  display: none;
+`;
+
+const CheckboxContainer = styled.span`
+  display: inline-block;
+  position: relative;
+  width: 20px;
+  height: 20px;
+  background-color: #fff;
+  border: 1px solid #cccccc;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const InnerCircle = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60%;
+  height: 60%;
+  background-color: #00b207;
+  border-radius: 50%;
+  opacity: 0;
+`;
+
 interface Props {
   products: Products[];
 }
@@ -29,16 +56,42 @@ const Filter = ({ products }: Props) => {
     "women's clothing",
   ];
 
+  // let mensClothingCount = 0;
+  // let electronicsCount = 0;
+  // let jeweleryCount = 0;
+  // let womansClothingCount = 0;
+
+  // products.map((item) => {
+  //   if (item.category === "electronics") {
+  //     electronicsCount++;
+  //   } else if (item.category === "jewelery") {
+  //     jeweleryCount++;
+  //   } else if (item.category === "men's clothing") {
+  //     mensClothingCount++;
+  //   } else if (item.category === "women's clothing") {
+  //     womansClothingCount++;
+  //   }
+  // });
+
   return (
     <div style={{ margin: "48px 100px 0px  300px" }}>
       <FilterButton>
         Filter <img src={FilterIcon} alt="" />
       </FilterButton>
       <div>
-        <h4>All Categories</h4>
-        {categories.map((item) => (
-          <p>{item}</p>
-        ))}
+        <h4 style={{ fontSize: "20px", margin: "24px 0px" }}>All Categories</h4>
+        <div style={{ display: "grid", gap: "14px" }}>
+          {categories.map((item, index) => (
+            <div style={{ display: "flex", gap: "10px" }}>
+              <label>
+                <StyledInput type="checkbox" />
+                <CheckboxContainer className="checkbox-container">
+                  <InnerCircle className="inner-circle"></InnerCircle>
+                </CheckboxContainer>
+              </label>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
